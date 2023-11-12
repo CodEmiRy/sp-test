8adc670d046a40d8a648971e590ea7a955783dfa
Emilia <rybicka.emilka@gmail.com>
2023-11-12 20:23:38 +0100

    Moduł2: Zarządzanie Konfiguracją 2

---
+++ src/main/java/pl/codemiry/sptest/AppInfo.java
@@ -0,0 +1,28 @@
+package pl.codemiry.sptest;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app-info")
+public class AppInfo {
+
+    private String appName;
+    private int appVersion;
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public void setAppVersion(int appVersion) {
+        this.appVersion = appVersion;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    private void get() {
+        System.out.println(appName);
+        System.out.println(appVersion);
+    }
+}
